d4a9dc95012115de442fd0e52a01a4066413ff04
Vladislav <vladbezv06@gmail.com>
2026-06-13 12:01:39 +0300

    выбор пасьянса

--- scripts/main-menu.gd
+++ scripts/main-menu.gd
@@ -18,7 +18,7 @@ func _on_quit_pressed() -> void:
 
 
 func _on_new_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Game.tscn")
+	get_tree().change_scene_to_file("res://scenes/Choice.tscn")
 
 
 func _on_leaderboard_pressed() -> void:
